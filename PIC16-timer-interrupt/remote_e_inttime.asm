3520538fc59531b9a5e019953c2289f31cb26a06
niratubertc <49430077+niratubertc@users.noreply.github.com>
2019-08-26 11:22:55 +0200

    Update remote_e_inttime.asm

--- PIC16-timer-interrupt/remote_e_inttime.asm
+++ PIC16-timer-interrupt/remote_e_inttime.asm
@@ -1,5 +1,5 @@
 
-	;  Copyright (C) 2019 HNira Tubert
+	;  Copyright (C) 2019 Nira Tubert
 	;	
 	;  This program is free software: you can redistribute it and/or
 	;  modify it under the terms of the GNU General Public License
